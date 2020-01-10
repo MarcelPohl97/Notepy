8b15c50bd89b164ea789a43ee2536a3b4e671161
Marcel <45712008+MarcelPohl97@users.noreply.github.com>
2020-01-10 23:26:52 +0100

    Finished UI

--- NotePy.py
+++ NotePy.py
@@ -30,22 +30,22 @@ class Frames:
     def __init__(self):
         self.notes_frame = Frame(window.root, bg="#343841", relief="sunken")
         self.notes_frame.place(width=270, height=300)
-        self.note_title = Label(self.notes_frame, text=menu.add_title_frame.get(), bg="#8F9297")
+        self.note_title = Label(self.notes_frame, text=menu.add_title_frame.get(), anchor="w", bg="#8F9297")
         self.note_title.place(relx=0, rely=0, relwidth=1, relheight=0.11)
-        self.deleteB = Button(self.notes_frame, text="X", image=icons.uncheck, command=self.delete_frame, bg="#8F9297")
-        self.deleteB.place(rely=0.01, relx=0.89)
+        self.deleteB = Button(self.note_title, text="X", image=icons.uncheck, command=self.delete_frame, bg="#8F9297")
+        self.deleteB.pack(side=RIGHT)
         self.addB = Button(self.notes_frame, text="ADD NOTE", image=icons.add_frame, command=self.add_note, bg="#8F9297")
-        self.addB.place(rely=0.91, relx=0.5)
+        self.addB.place(rely=0.12, relx=0.733)
         self.deleteN = Button(self.notes_frame, text="DEL-N", image=icons.delete, command=self.del_Note, bg="#8F9297")
-        self.deleteN.place(rely=0.91, relx=0.70)
+        self.deleteN.place(rely=0.12, relx=0.843)
         self.note_list = Listbox(self.notes_frame, bg="#8F9297", relief="ridge")
-        self.note_list.place(rely=0.2, relx=0.05, relheight=0.7, relwidth=0.9)
+        self.note_list.place(rely=0.22, relx=0.05, relheight=0.7, relwidth=0.9)
         self.entry_list = Entry(self.notes_frame)
-        self.entry_list.place(rely=0.91, relx=0.05)
+        self.entry_list.place(rely=0.118, relx=0.27, height=30)
         self.check_Note = Button(self.notes_frame, text="Check", image=icons.add, command=self.check_note, bg="#8F9297")
-        self.check_Note.place(rely=0.3, relx=0.53)
+        self.check_Note.place(rely=0.12, relx=0.05)
         self.uncheck_Note = Button(self.notes_frame, text="Uncheck", image=icons.uncheck2, command=self.uncheck_note, bg="#8F9297")
-        self.uncheck_Note.place(rely=0.3, relx=0.73)
+        self.uncheck_Note.place(rely=0.12, relx=0.163)
 
     def delete_frame(self):
         self.notes_frame.destroy()
@@ -99,19 +99,29 @@ class Menu:
                          "800x600": "800x600"}
         self.size = StringVar(window.root)
         self.app_size = OptionMenu(self.header, self.size, *self.size_options, command=self.set_window_size)
-        self.app_size.pack(side=RIGHT, padx=1.5)
+        self.app_size.pack(side=RIGHT)
         self.app_size.config(indicatoron=0, image=icons.menu, bg='#EBECF0')
         self.add_note_frame = Button(self.header, bg="#EBECF0", image=icons.add_window, command=self.add_frames)
-        self.add_note_frame.pack(side=RIGHT, padx=1.5)
+        self.add_note_frame.pack(side=RIGHT)
         self.add_title_frame = Entry(self.header, width=25)
-        self.add_title_frame.pack(side=RIGHT, padx=1.5)
+        self.add_title_frame.pack(side=RIGHT, fill=Y,padx=1)
         self.about = Button(self.header, bg="#EBECF0", image=icons.about)
         self.about.pack(side=LEFT)
         self.info = Button(self.header, bg="#EBECF0", image=icons.info)
         self.info.pack(side=LEFT)
-        self.color_change= OptionMenu(self.header, self.size, *self.size_options)
-        self.color_change.pack(side=LEFT, padx=1.5)
+        self.size_options = ["orange", "blue", "red", "green", "purple", "lightblue"]
+        self.get_colors = {"orange": ["#B37B2C", "#D29034"],
+                         "blue": ["#0067A3", "#0079BF"],
+                         "red": ["#963C2B", "#B04632"],
+                         "green": ["#458131", "#519839"],
+                         "purple": ["#755286", "#89609E"],
+                         "lightblue": ["#0094AE", "#00AECC"]}
+        self.background = StringVar(window.root)
+        self.color_change= OptionMenu(self.header, self.background, *self.get_colors, command=self.change_bg)
+        self.color_change.pack(side=LEFT)
         self.color_change.config(indicatoron=0, image=icons.change_bg, bg='#EBECF0')
+        self.chat_open = Button(self.header, bg="#EBECF0", image=icons.chat)
+        self.chat_open.pack(side=LEFT, padx=1)
 
 
     def add_frames(self):
@@ -122,6 +132,12 @@ class Menu:
         if action:
             window.root.geometry(action)
             window.root.resizable(0, 0)
+
+    def change_bg(self, bg):
+        self.background_color = self.get_colors.get(bg)
+        if self.background_color:
+            self.header["bg"] = self.background_color[0]
+            window.root.configure(bg=self.background_color[1])
             
                 
         
